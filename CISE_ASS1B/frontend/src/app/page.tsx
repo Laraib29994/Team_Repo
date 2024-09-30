d7e8bccca0e7b03d695e1aa34cfd57178ec32080
Peter <112389648+Guttami@users.noreply.github.com>
2024-09-30 16:43:12 +1300

    Created a search bar and a filter by date

--- CISE_ASS1B/frontend/src/app/page.tsx
+++ CISE_ASS1B/frontend/src/app/page.tsx
@@ -1,11 +1,32 @@
 'use client'
 
+import { useState, useEffect} from 'react';
 import ShowBookList from "@/components/ShowBookList";
+import { Book } from "@/components/Book";
+
+
 
 export default function Home() {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [filteredBooks, setFilteredBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8082/api/books')
+    .then((res) => res.json())
+    .then((data) => {
+      setBooks(data);
+      setFilteredBooks(data);
+    })
+    .catch((err) => {
+      console.log('Error fetching books: ' + err);
+    });
+
+  }, []);
+
+  
   return (
     <main>
-      <ShowBookList />
+      <ShowBookList books={filteredBooks} />      
     </main>
   );
 }
--- CISE_ASS1B/frontend/src/components/ShowBookList.tsx
+++ CISE_ASS1B/frontend/src/components/ShowBookList.tsx
@@ -2,7 +2,107 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import BookCard from './BookCard';
 import { Book } from './Book';
+import SearchBar from './SearchBar';
+import DateFilter from './DateFilter';
 
+
+interface ShowBookListProps {
+  books: Book[];
+}
+
+
+const ShowBookList: React.FC<ShowBookListProps> = ({ books }) => {
+  const [filteredBooks, setFilteredBooks] = useState<Book[]>(books);
+
+  useEffect(() => {
+      setFilteredBooks(books); // Initialize with all books
+  }, [books]);
+
+  const bookList = filteredBooks.length === 0
+      ? 'There is no book record!'
+      : filteredBooks.map((book, k) => <BookCard book={book} key={k} />);
+
+  return (
+      <div className='ShowBookList'>
+          <div className='container'>
+              <div className='row'>
+                  <div className='col-md-12'>
+                      <br />
+                      <h2 className='display-4 text-center'>Books List</h2>
+                  </div>
+
+                  <div className='col-md-11'>
+                      <Link href='/create-book' className='btn btn-outline-warning float-right'>
+                          + Add New Book
+                      </Link>
+                      <br />
+                      <br />
+                      <hr />
+                  </div>
+              </div>
+
+
+              <div className='SearchBar'>
+                <SearchBar books={books} setFilteredBooks={setFilteredBooks}/>
+              </div>
+
+              {/* Add DateFilter component */}
+              <div className="date-filter">
+                <DateFilter books={books} setFilteredBooks={setFilteredBooks} />
+              </div>
+
+              <div className='list'>{bookList}</div>
+          </div>
+      </div>
+  );
+};
+
+export default ShowBookList;
+
+
+
+
+
+/*
+const ShowBookList: React.FC<ShowBookListProps> = ({ books }) => {
+  const [filteredBooks, setFilteredBooks] = useState<Book[]>(books);
+
+  const bookList = books.length === 0
+    ? 'There is no book record!'
+    : books.map((book, k) => <BookCard book={book} key={k} />);
+
+  return (
+    <div className='ShowBookList'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <br />
+            <h2 className='display-4 text-center'>Books List</h2>
+          </div>
+
+          <div className='col-md-11'>
+            <Link href='/create-book' className='btn btn-outline-warning float-right'>
+              + Add New Book
+            </Link>
+            <br />
+            <br />
+            <hr />
+          </div>
+        </div>
+
+        <div className='SearchBar'>
+          <SearchBar books={books} setFilteredBooks={setFilteredBooks}/>
+        </div>
+
+        <div className='list'>{bookList}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ShowBookList;
+
+/*
 function ShowBookList() {
   const [books, setBooks] = useState<[Book?]>([]);
 
@@ -22,7 +122,7 @@ function ShowBookList() {
   const bookList =
     books.length === 0
       ? 'there is no book record!'
-      : books.map((book, k) => <BookCard book={book} key={k} />);
+      : books.map((book, k) => <BookCard book={book} key={k} />);                                                                           
 
       return (
         <div className='ShowBookList'>
@@ -53,4 +153,4 @@ function ShowBookList() {
     }
     
     export default ShowBookList;
-    
+  */
