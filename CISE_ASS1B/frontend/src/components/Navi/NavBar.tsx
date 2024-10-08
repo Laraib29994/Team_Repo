9294917321142a01887779e62e41716194fcb1b0
JuliusCanamo <jqd2690@autuni.ac.nz>
2024-10-08 20:12:38 +1300

    Please test the Navi folder code

---
+++ CISE_ASS1B/frontend/src/components/Navi/NavBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import NavDropdown from './NavDropdown';
+import styles from './Nav.module.scss';
+
+const NavBar: React.FC = () => {
+    const router = useRouter();
+    const getActiveClass = (path: string) => router.pathname === path ? styles.active : '';
+
+    return (
+        <nav className={styles.nav}>
+            <ul className={styles.navList}>
+                <NavItem route="/" className={getActiveClass('/')}>Home</NavItem>
+                <NavItem route="/about" className={getActiveClass('/about')}>About</NavItem>
+                <NavItem route="/contact" className={getActiveClass('/contact')}>Contact</NavItem>
+                <NavDropdown />
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
