8f1098c88b8490ae41a1af2e37e4d1d310e25628
JuliusCanamo <jqd2690@autuni.ac.nz>
2024-10-13 15:45:09 +1300

    Menu bar done

--- CISE_ASS1B/frontend/src/app/Home/page.tsx
+++ CISE_ASS1B/frontend/src/app/Home/page.tsx
@@ -1,12 +1,13 @@
-'use client'
+'use client'; // This is required to use React features in this component
 
-import ShowArticleList from "@/./components/ShowArticleList";
-//import Navbar from "@/components/Nav/NavBar.tsx";
+import ShowArticleList from "@/components/ShowArticleList"; // Import your article list component
+import Header from "@/components/Nav/Header"; // Import the Header component
 
 export default function Home() {
   return (
     <main>
-      <ShowArticleList/>
+      <Header /> {/* This will render the menu bar */}
+      <ShowArticleList /> {/* This will render your article list */}
     </main>
   );
 }
