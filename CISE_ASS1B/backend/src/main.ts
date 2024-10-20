bd33056afaa48459efeb7077fbac7217cbab3b62
Laraib29994 <ksk0308@aut.ac.nz>
2024-10-21 00:23:51 +1300

    [CORS][Updated cors link]

--- CISE_ASS1B/backend/src/main.ts
+++ CISE_ASS1B/backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // enable cors
-  app.enableCors({ origin: true, credentials: true });
+  app.enableCors({ origin: 'https://frontend-6j3gyq7hk-laraib2994s-projects.vercel.app', credentials: true });
   const port = process.env.PORT || 8082;
   await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
